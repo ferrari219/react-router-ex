6e6734d2ecf0d673ef6a34995ac8d995d9ed9366
MakeDesire <ferrari219@nate.com>
2018-11-08 18:05:52 +0900

    ..

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>리액트 라우터 소개</p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>가장 처음에 보여짐</p>
+        </div>
+    );
+};
+
+export default Home;
